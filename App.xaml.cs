14e8da34dd7c9692ba58e0fc8a55ad687ef6b5f0
Adrian Smith <adrian.smith@civica.co.uk>
2024-07-05 15:00:04 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiBugDemoApp2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace MauiBugDemoApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        int enabledCount = 0;
+        int disabledCount = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var enabledTgr = new TapGestureRecognizer();
+            enabledTgr.Tapped += (s, e) =>
+            {
+                enabledCount++;
+                lblEnabledCount.Text = $"Enabled label taps: {enabledCount}";
+            };
+            lblEnabled.GestureRecognizers.Add(enabledTgr);
+
+            var disabledTgr = new TapGestureRecognizer();
+            disabledTgr.Tapped += (s, e) =>
+            {
+                disabledCount++;
+                lblDisabledCount.Text = $"Disabled label taps: {disabledCount}";
+            };
+            lblDisabled.GestureRecognizers.Add(disabledTgr);
+        }
+    }
+}
